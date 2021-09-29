942ae1569b6f0e5b8d4a45ab2249727c06d2aabc
liaozhenyu_brian <brianliu1229@outlook.com>
2021-09-29 17:35:32 +0800

    enable spring actuator endpoints

--- src/main/java/com/yu/AppSecurityConfig.java
+++ src/main/java/com/yu/AppSecurityConfig.java
@@ -42,6 +42,10 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers(apiBaseUrl+"/debug/**").permitAll();
         }
 
+        // actuator endpoints
+        http.authorizeRequests()
+                .antMatchers("/actuator/**").permitAll();
+
         // access control for endpoints
         if (disablePermissionCheck) {
             logger.warn("app started with disablePermissionCheck.");
